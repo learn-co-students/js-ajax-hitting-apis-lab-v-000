971a6a38799c4ba3fa3c5b4df7b07cd31344c96f
dustin <dandersonsoftware@outlook.com>
2017-10-11 15:21:26 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,45 @@
+function getRepositories() {
+  const username = document.getElementById("username").value;
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayRepositories)
+  req.open("GET", 'https://api.github.com/users/' + username + '/repos')
+  req.send();
+}
+
+function displayRepositories() {
+  let repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.full_name + '" onclick="getCommits(this)">Get Commits</a>'+ ' - <a href="#" data-repo="' + r.full_name + '" onclick="getBranches(this)">Get Branches</a>' + ' - <a href="'+ r.html_url +'">View on GitHub</a>'+ '</li>').join('')}</ul>`;
+  document.getElementById("repositories").innerHTML = repoList;
+}
+
+function getCommits(el){
+  const repoName = el.dataset.repository
+  const name = el.dataset.username
+
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayCommits)
+  req.open("GET", `https://api.github.com/repos/${name}/${repoName}/commits`)
+  req.send()
+}
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
+}
+
+function getBranches(el) {
+  const repoName = el.dataset.repository
+  const name = el.dataset.username
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayBranches)
+  req.open("GET", `https://api.github.com/repos/${name}/${repoName}/branches`)
+  req.send()
+}
+
+function displayBranches() {
+  const branches = JSON.parse(this.responseText)
+  const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = branchesList
+}
