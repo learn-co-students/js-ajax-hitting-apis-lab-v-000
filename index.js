de6e19159aab1bf463c9e34d58de29abe079c1c1
laramontana <larabrightside@gmail.com>
2017-11-13 20:15:09 +0300

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function displayRepositories(event) {
 
   var repos = JSON.parse(this.responseText);
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" data-username="' + r.owner.login + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repository="' + r.name + '" data-username="' + r.owner.login + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`;
   document.getElementById("repositories").innerHTML = repoList;
 }
 
@@ -15,16 +15,16 @@ function getRepositories() {
 
 function displayCommits() {
   const commits = JSON.parse(this.responseText)
-
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.commit.author.name + '</strong> - (@' + commit.author.login + ") " + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
 }
 
 function getCommits(el) {
-  const repo = el.dataset.repo //Here we grab that data-repo value through the dataset property,
+  debugger
+  const repository = el.dataset.repository //Here we grab that data-repo value through the dataset property,
   const username = el.dataset.username
   const req = new XMLHttpRequest() //then set up an XHR request,
   req.addEventListener("load", displayCommits) //with an event listener and callback function, just like we did in getRepositories
-  req.open("GET", `https://api.github.com/repos/${username}/${repo}/commits`)
+  req.open("GET", `https://api.github.com/repos/${username}/${repository}/commits`)
   req.send()
 }
