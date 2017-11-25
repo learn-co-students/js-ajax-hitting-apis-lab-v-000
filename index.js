3bb2d4b2fc240ff06c4a24db75f4d6dc523e7acf
Jim Stricker <jmstricker93@gmail.com>
2017-11-25 13:03:12 -0500

    display commits functionality working

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
 
-function showRepositories(event) {
-  debugger
+function displayRepositories(event) {
+  // //debugger
+  let username = document.getElementById('username').value
 
   let repos = JSON.parse(this.responseText)
   console.log(repos)
-
-  let repolist = `<ul>${repos.map(r => '<li>' + r.name + '- <a href="' + r.html_url + '">Link to Repo</a> - <a href="#" onclick="getCommits()"> Show Commits</a>' + '</li>').join('')}</ul>`
-  debugger
+  //debugger
+  let repolist = `<ul>${repos.map(r =>{return '<li>' + r.name + '- <a href="' + r.html_url + '">Link to Repo</a> - <a href="#" data-repository="' + r.name + '" data-username="' + r.owner.login + '" onclick="getCommits(this); return false;"> Show Commits</a>' + '</li>'}).join('')}</ul>`
+  // debugger
   document.getElementById('repositories').innerHTML = repolist
 }
 
 function getRepositories(){
   let username = document.getElementById('username').value
   const req = new XMLHttpRequest()
-  debugger
-  req.addEventListener("load", showRepositories);
-  debugger
+  // debugger
+  req.addEventListener("load", displayRepositories);
+  //debugger
   req.open("GET", `https://api.github.com/users/${username}/repos`)
-  debugger
+  //debugger
   req.send()
 }
 
 
-function showCommits(){
+function displayCommits(){
+  let commits = JSON.parse(this.responseText)
+  console.log(commits)
 
+  let commitList = `<ul>${commits.map(c => '<li>' + c.commit.committer.name + ' - ' + c.committer.login + ' - ' + c.commit.message).join('')} </ul>`
+
+  document.getElementById('details').innerHTML = commitList
 }
 
-function getCommits(){
-
+function getCommits(el){
+  console.log(el)
+  const repoName = el.dataset.repository
+  const ownerName = el.dataset.username
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayCommits)
+  req.open("GET", `https://api.github.com/repos/${ownerName}/${repoName}/commits`)
+  req.send()
 }
