51ccf462ddd0031a36f4cd00e3c837d9b09f9994
Ricardo <github email address>
2017-11-26 12:55:28 -0500

    getBranches is functional

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 //include name and link to the public URL
 function getRepositories() {
   let username = document.getElementById('username').value;
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", showRepositories)
-  req.open("GET", `https://api.github.com/users/${username}/repos`)
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", showRepositories);
+  req.open("GET", `https://api.github.com/users/octocat/repos`);
   req.send()
 }
 
 function showRepositories(event, data) {
-  const repos = JSON.parse(this.responseText)
+  const repos = JSON.parse(this.responseText);
   const repoList = `<ul>${repos.map(repo => '<li><a href="' + repo.html_url + '">'+ repo.name +'</a> - <a href="#" data-repo="' + repo.name + '" onclick="getCommits(this)">Get Commits</a> - <a href="#" data-repo="' + repo.name + '" data-owner="' + repo.owner.login + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
@@ -24,16 +24,23 @@ function getCommits(el) {
 
 function showCommits() {
   const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '-'  + commit.commit.author.name  + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + ' - '  + commit.commit.author.name  + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
 }
 
 // GET /repos/:owner/:repo/branches
 
 function getBranches(el) {
-  const ownerName = el.datset.owner
+  const ownerName = el.dataset.owner
   const repoName = el.dataset.repo
   const req = new XMLHttpRequest()
+  req.addEventListener("load", displayBranches)
   req.open("GET", `https://api.github.com/repos/${ownerName}/${repoName}/branches`)
   req.send()
 }
+
+function displayBranches() {
+	// const branches = JSON.parse(this.responseText);
+	// const branchesList = `<ul>${branches.map(branch => '<li><strong>' + commit.author.login + ' - '  + commit.commit.author.name  + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+}
+ 
