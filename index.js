5852a69f26e627602135e4924a468e489eed6961
Alexandra <github email address>
2017-11-29 08:49:53 -0800

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 function getRepositories(){
-
+  username = document.getElementById("git-user").value
+  let api_query = 'https://api.github.com/users/' + username + '/repos'
+  debugger
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories)
+  req.open("GET", api_query)
+  req.send()
 }
 
-function showRepositories(){
+function showRepositories(event, data){
+  let repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(repo => '<li>' + repo.name +
+  ' - <a href="#" data-repo="' + repo.name +
+  '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function getCommits(el) {
 
 }
