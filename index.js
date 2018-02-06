128636c6ef41c1fab3f4b10c96cdf8a26f1d202c
github username pianistps <pianistps@yahoo.com>
2018-02-05 22:47:36 -0600

    added get commits and display commits functions

--- index.js
+++ index.js
@@ -3,12 +3,32 @@ const giturl = "https://api.github.com/"
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList =
-    `<ul>${repos.map(r =>
-      const dataUsername = 'data-username="' + r.owner.login + '"'
-      '<li>' + r.name + '</li>').join('')}
-     </ul>`
-  document.getElementById("repositories").innerHTML = repoList
+  const repoList = "<ul>" + repos.map(repo => {
+     const dataUsername = 'data-username="' + repo.owner.login + '"'
+     const dataRepoName = 'data-repository="' + repo.name + '"'
+     return(`<li>
+             <h2>${repo.name}</h2>
+             <a href="${repo.html_url}">${repo.html_url}</a><br>
+             <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+             </li>`)
+   }).join('') + "</ul>";
+   document.getElementById("repositories").innerHTML = repoList
+}
+
+function displayCommits() {
+  var commits = JSON.parse(this.responseText)
+  console.log(commits)
+  const commitList = "<ul>" + commits.map(commit => {
+    const dataUsername = '"' + commit.author.login + '"'
+    const dataCommitMessage = '"' + commit.commit.message + '"'
+    const dataFullName = '"' + commit.commit.author.name + '"'
+    return (`<li>
+        Github Name: ${dataUsername}<br>
+        Author Name:  ${dataFullName}<br>
+        Commit Message: ${dataCommitMessage}<br>
+      </li>`)}).join('') + "</ul>"
+
+    document.getElementById("details").innerHTML = commitList
 }
 
 function getRepositories() {
@@ -17,5 +37,14 @@ function getRepositories() {
   var user = document.getElementById("username").value
   var uri = giturl + "users/" + user + "/repos"
   req.open("GET", uri)
+  req.send()
+}
+
+function getCommits(c) {
+  const req = new XMLHttpRequest()
+  ///repos/:owner/:repo/commits
+  req.addEventListener("load", displayCommits)
+  var uri = giturl + "repos/" + c.dataset.username + "/" + c.dataset.repository + "/commits"
+  req.open("Get", uri)
   req.send()
 }
