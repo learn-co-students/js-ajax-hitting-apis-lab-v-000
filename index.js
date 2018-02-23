e1973c57b2718209528a768ae9f9a5628c1877d8
Terry Blue <terryblue@comcast.net>
2018-02-23 23:42:03 +0000

    Done.

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ function getRepositories() {
 
 function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
-  console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="https://github.com/octocat/'+r.name+
-  '" target="_blank">Get Repo</a> - <a href="#" data-repo="' + r.name +
+  '" target="_blank">Get Repo</a> - <a href="#" data-repository="' + r.name +
   '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
 function getCommits(el) {
-  const name = el.dataset.repo
+  const name = el.dataset.repository
+  console.log("Repository: " + name)
   const req = new XMLHttpRequest()
-  var userName = document.getElementById("username").value;
+  var userName = document.getElementById("username").value
   req.addEventListener("load", displayCommits)
-  req.open("GET", `https://api.github.com/repos/${userName}/`+name+'/commits')
+  req.open("GET", `https://api.github.com/repos/${userName}/` + name + '/commits')
   req.send()
 }
 
