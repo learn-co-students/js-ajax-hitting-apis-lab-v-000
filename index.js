8e3d1540817fd2ad494ed902f8a5f1468676700d
Terry Blue <terryblue@comcast.net>
2018-02-23 20:07:51 +0000

    Done.

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ function getRepositories() {
 }
 
 function displayRepositories(event, data) {
-  //this is set to the XMLHttpRequest object that fired the event
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  // const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="https://github.com/octocat/'+r.name+'">Get Repo</a></li>').join('')}</ul>`
-  // https://github.com/octocat/Hello-World
   document.getElementById("repositories").innerHTML = repoList
 }
 
