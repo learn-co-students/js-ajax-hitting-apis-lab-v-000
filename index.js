c1dd0161967dbebd4de75b51b8d221872b4def7e
Terry Blue <terryblue@comcast.net>
2018-02-23 23:44:16 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ function getRepositories() {
 
 function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="https://github.com/octocat/'+r.name+
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name +
+  ' - <a href="https://github.com/octocat/'+r.name+
   '" target="_blank">Get Repo</a> - <a href="#" data-repository="' + r.name +
   '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
@@ -26,7 +27,9 @@ function getCommits(el) {
 
 function displayCommits() {
   const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + ' - ' + commit.commit.author.name + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' +
+  commit.author.login + ' - ' + commit.commit.author.name + '</strong> - ' +
+  commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
 }
 
