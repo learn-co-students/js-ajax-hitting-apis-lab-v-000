3f9238f4713293ca5973d6c42edbb7432f027e8c
Terry Blue <terryblue@comcast.net>
2018-02-23 22:08:29 +0000

    Done.

--- index.js
+++ index.js
@@ -9,19 +9,22 @@ function getRepositories() {
 function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="https://github.com/octocat/'+r.name+'" target="_blank">Get Repo</a></li>').join('')}</ul>`
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="https://github.com/octocat/'+r.name+'" target="_blank">Get Repo</a> - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
 function getCommits(el) {
   const name = el.dataset.repo
   const req = new XMLHttpRequest()
-  req.open("GET", 'https://api.github.com/repos/octocat/Spoon-Knife/commits')
+  req.addEventListener("load", displayCommits)
+  req.open("GET", `https://api.github.com/repos/${userName}/`+name+'/commits')
   req.send()
 }
 
 function displayCommits() {
-
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
 }
 
 function getBranches() {
