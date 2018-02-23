5537902c8253017bd5247fc30d303f851a4756bf
Terry Blue <terryblue@comcast.net>
2018-02-23 19:20:14 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 function getRepositories() {
   const req = new XMLHttpRequest()
-  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  var userName = document.getElementById("username").value;
+  req.addEventListener("load", displayRepositories);
+  req.open("GET", `https://api.github.com/users/${userName}/repos`)
   req.send()
+}
+
+function displayRepositories(event, data) {
+  //this is set to the XMLHttpRequest object that fired the event
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
 }
 
 function getCommits(el) {
