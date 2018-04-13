0c5a0993fbb725e61f3cb33b39788d4d46221478
github username <github email address>
2018-04-13 01:09:14 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,49 @@
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  const username = document.getElementById('username').value
+  req.addEventListener('load', displayRepositories)
+  req.open("GET", `https://api.github.com/users/${username}/repos`)
+  req.send()
+}
+
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText)
+  const repoList = repos.map(r => {
+    return `<li><a href=${r.html_url}>${r.name}</a><br>
+    <a href='#' data-respository="${r.name}" data-username="${r.owner.login}"
+    onclick='getCommits(this)'> Show Commits </a>
+    <a href='#' data-respository="${r.name}" data-username="${r.owner.login}"
+    onclick='getBranches(this)'> Show Branches </a></li>`
+  }).join('')
+  document.getElementById("repositories").innerHTML = repoList
+}
+
+function getCommits(el) {
+  const repo = el.dataset.repository;
+  const username = el.dataset.username;
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayCommits)
+  req.open("GET", `https://api.github.com/repos/${username}/${repo}/commits`)
+  req.send()
+}
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li>' + commit.commit.author.name + ' (' + commit.author.login + ')' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
+}
+
+function getBranches(el) {
+  const repo = el.dataset.repository;
+  const username = el.dataset.username;
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayBranches)
+  req.open("GET", `https://api.github.com/repos/${username}/${repo}/branches`)
+  req.send()
+}
+
+function displayBranches() {
+  const branches = JSON.parse(this.responseText)
+  const branchesList = `<ul>${branches.map(branch => `<li>${branch.name}</li>`).join('')}`
+  document.getElementById('details').innerHTML = branchesList
+}
