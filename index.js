07ebe65871027f61940909f328398561e29acebe
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-04-17 23:42:41 +0000

    Done.

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function displayRepositories(){
         <h2>${repo.name}</h2>
         <a href="${repo.html_url}">${repo.html_url}</a><br>
         <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+        <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
       </li>`
     )
   }).join('') + "</ul>";
@@ -39,4 +40,19 @@ function displayCommits() {
   const commits = JSON.parse(this.responseText)
   const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
+}
+
+function getBranches(el){
+  const repoName = el.dataset.repository
+  const uri = rootURL + "/repos/" + el.dataset.username + "/" + repoName + "/branches"
+  const xhr = new XMLHttpRequest()
+  xhr.addEventListener("load", displayBranches)
+  xhr.open("GET", uri)
+  xhr.send()
+}
+
+function displayBranches(){
+  const branches = JSON.parse(this.responseText)
+  const branchesList = `<ul>${branches.map(branch => 'li' + branch.name + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = branchesList
 }
