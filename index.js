cb5fa15b98e14a012dabb9c9f88db982173d3daa
Anastasia  Barkett <anastasia.barkett@gmail.com>
2018-04-17 23:32:08 +0000

    Done.

--- index.js
+++ index.js
@@ -18,9 +18,25 @@ function displayRepositories(){
     return(`
       <li>
         <h2>${repo.name}</h2>
-
+        <a href="${repo.html_url}">${repo.html_url}</a><br>
+        <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
       </li>`
     )
   }).join('') + "</ul>";
   document.getElementById("repositories").innerHTML = repoList
+}
+
+function getCommits(el){
+  const repoName = el.dataset.repository
+  const uri = rootURL + "/repos/" + el.dataset.username + "/" + repoName + "/commits"
+  const xhr = new XMLHttpRequest()
+  xhr.addEventListener("load", displayCommits)
+  xhr.open("GET", uri)
+  xhr.send()
+}
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
 }
