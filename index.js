0f184af31ac2b9d9d2e03475b188bc54c572a03b
jacobsilver2 <jacobsilver2@mac.com>
2018-05-13 11:48:38 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+// Create a form with a username field that calls a getRepositories function that loads the repositories div with a list of public repositories for that user. The displayed repositories should include the name and a link to the URL (HTML URL, not API URL).
+
+// Add a link to each repository that calls a getCommits function on click and, when the request is complete, calls a displayCommits function that fills the details div with a list of commits for that repository. The display of commits should include the author's Github name, the author's full name, and the commit message. Give the link data attributes of username and repository to be used by the getCommits function.
+
+
+// Add a link to each repository that calls a getBranches function when clicked and, when complete, calls a displayBranches function that fills the details div with a list of names of each branch of the repository. Give the link data attributes of username and repository for use by the getBranches function.
+
+function getRepositories() {
+  let userName = document.getElementById('user_name').value;
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/' + userName + '/repos');
+  req.send()
+}
+
+
+function showRepositories(event, data) {
+  var repos = JSON.parse(this.responseText)
+  const repoList = `<ul>${repos.map(r => '<li>' + '<a href="' + r.html_url + '">' + r.name + '</a>' + ' - <a href="#" data-username="' + r.owner.login + '" data-repo="' + r.name + '" + onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList;
+}
+
+function getCommits(el) {
+  const userName = el.dataset.username
+  const repoName = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showCommits)
+  req.open("GET", 'https://api.github.com/repos/' + userName + '/' + repoName + '/commits')
+  req.send()
+}
+
+function showCommits() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById('details').innerHTML = commitsList
+}
+
