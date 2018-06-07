67519c954ed338b5c2a29bc912dc64568f194ffe
Olena Ageyeva <eageeva@hotmail.com>
2018-06-07 12:43:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+function getRepositories() {
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showRepositories);
+  req.open("GET", 'https://api.github.com/users/octocat/repos')
+  req.send()
+}
+
+function showRepositories(event, data) {
+  //this is set to the XMLHttpRequest object that fired the event
+  var repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
