f592770fa35ec689e47184b7c8ac37afbf7857e2
Dan Suarez <suarez.daniel101@gmail.com>
2018-07-17 03:03:05 +0000

    Done.

--- index.js
+++ index.js
@@ -47,14 +47,14 @@ function getBranches(el) {
   const name = el.dataset.repository
   const req = new XMLHttpRequest()
   req.addEventListener("load", displayBranches)
-  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/branches')
   req.send()
 }
 
 function displayBranches() {
-  const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li>' + commit.author.login + ' <strong>' + commit.commit.author.name + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
-  document.getElementById("details").innerHTML = commitsList
+  const branches = JSON.parse(this.responseText)
+  const branchesList = `<ul>${branches.map(branch => '<li>' '<strong>' + branch.name + '</strong> - ' '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = branchesList
 }
 
 // Add a link to each repository that calls a getBranches function when clicked and, when complete,
