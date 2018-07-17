0cfc43bde267f904f3cbdf776bc55bc715db0fd4
Dan Suarez <suarez.daniel101@gmail.com>
2018-07-17 02:40:50 +0000

    Done.

--- index.js
+++ index.js
@@ -37,12 +37,34 @@ function getCommits(el) {
   req.send()
 }
 
-
 function displayCommits() {
   const commits = JSON.parse(this.responseText)
   const commitsList = `<ul>${commits.map(commit => '<li>' + commit.author.login + ' <strong>' + commit.commit.author.name + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("details").innerHTML = commitsList
 }
+
+function getBranches(el) {
+  const name = el.dataset.repository
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", displayBranches)
+  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+  req.send()
+}
+
+function displayBranches() {
+  const commits = JSON.parse(this.responseText)
+  const commitsList = `<ul>${commits.map(commit => '<li>' + commit.author.login + ' <strong>' + commit.commit.author.name + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+  document.getElementById("details").innerHTML = commitsList
+}
+
+// Add a link to each repository that calls a getBranches function when clicked and, when complete,
+// calls a displayBranches function that fills the details div with a list of names of each branch of the repository.
+// Give the link data attributes of username and repository for use by the getBranches function.
+//
+// Add a link to each repository that calls a getCommits function on click and, when the request is complete,
+// calls a displayCommits function that fills the details div with a list of commits for that repository.
+// The display of commits should include the author's Github name, the author's full name, and the commit message.
+// Give the link data attributes of username and repository to be used by the getCommits function.
 
 // function showCommits() {
 //   const commits = JSON.parse(this.responseText)
