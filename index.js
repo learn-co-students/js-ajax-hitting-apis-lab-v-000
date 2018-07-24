1393783a113ca51d41d70745a941cf02a5da7d81
Dan Suarez <suarez.daniel101@gmail.com>
2018-07-24 01:59:03 +0000

    Done.

--- index.js
+++ index.js
@@ -5,57 +5,41 @@ function getRepositories() {
   req.send()
 }
 
-// function showRepositories(event, data) {
-//   console.log(this.responseText)
-//   let repoList = "<ul>"
-//   for(var i=0;i < this.responseText.length; i++) {
-//     repoList += "<li>" + this.responseText[i]["name"] + "</li>"
-//   }
-//   repoList += "</ul>"
-//   document.getElementById("repositories").innerHTML = repoList
-// }
 
 // function showRepositories(event, data) {
 //   var repos = JSON.parse(this.responseText)
 //   console.log(repos)
-//   const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`
+//   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repository="' + r.name + '" onclick="getCommits(this)">Get Commits</a> - <a href="#" data-username="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
 //   document.getElementById("repositories").innerHTML = repoList
 // }
-
-function showRepositories(event, data) {
-  var repos = JSON.parse(this.responseText)
-  console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repository="' + r.name + '" onclick="getCommits(this)">Get Commits</a> - <a href="#" data-username="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
-  document.getElementById("repositories").innerHTML = repoList
-}
-
-function getCommits(el) {
-  const name = el.dataset.repository
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", displayCommits)
-  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
-  req.send()
-}
-
-function displayCommits() {
-  const commits = JSON.parse(this.responseText)
-  const commitsList = `<ul>${commits.map(commit => '<li>' + commit.author.login + ' <strong>' + commit.commit.author.name + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
-  document.getElementById("details").innerHTML = commitsList
-}
-
-function getBranches(el) {
-  const name = el.dataset.username
-  const req = new XMLHttpRequest()
-  req.addEventListener("load", displayBranches)
-  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/branches')
-  req.send()
-}
-
-function displayBranches() {
-  const branches = JSON.parse(this.responseText)
-  const commitsList = `<ul>${branches.map(commit => '<li>' + commit.author.login + ' <strong>' + commit.commit.author.name + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
-  document.getElementById("details").innerHTML = commitsList
-}
+//
+// function getCommits(el) {
+//   const name = el.dataset.repository
+//   const req = new XMLHttpRequest()
+//   req.addEventListener("load", displayCommits)
+//   req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+//   req.send()
+// }
+//
+// function displayCommits() {
+//   const commits = JSON.parse(this.responseText)
+//   const commitsList = `<ul>${commits.map(commit => '<li>' + commit.author.login + ' <strong>' + commit.commit.author.name + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+//   document.getElementById("details").innerHTML = commitsList
+// }
+//
+// function getBranches(el) {
+//   const name = el.dataset.username
+//   const req = new XMLHttpRequest()
+//   req.addEventListener("load", displayBranches)
+//   req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
+//   req.send()
+// }
+//
+// function displayBranches() {
+//   const commits = JSON.parse(this.responseText)
+//   const commitsList = `<ul>${commits.map(commit => '<li>' + commit.author.login + ' <strong>' + commit.commit.author.name + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+//   document.getElementById("details").innerHTML = commitsList
+// }
 
 // Add a link to each repository that calls a getBranches function when clicked and, when complete,
 // calls a displayBranches function that fills the details div with a list of names of each branch of the repository.
