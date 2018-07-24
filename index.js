256c17b140ebb1b5524e4ab1441d283665282863
Dan Suarez <suarez.daniel101@gmail.com>
2018-07-24 21:06:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 function getRepositories() {
   const req = new XMLHttpRequest()
-  req.addEventListener("load", showRepositories);
+  req.addEventListener("load", displayRepositories);
   req.open("GET", 'https://api.github.com/users/octocat/repos')
   req.send()
 }
 
 
-function showRepositories(event, data) {
+function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repository="' + r.name + '" onclick="getCommits(this)">Get Commits</a> - <a href="#" data-username="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
