81b7f6a0237d11503ee5ec04dda7aab3f387a26b
Dan Suarez <suarez.daniel101@gmail.com>
2018-07-24 22:01:23 +0000

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function getRepositories() {
 function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = '<ul><li>Hello-World - <a href="#" data-repository="’ + r.name + ‘" onclick="getCommits(this)">Get Commits</a> - <a href="#" data-username="’ + r.name + ‘" onclick="getBranches(this)">Get Branches</a></li></ul>'
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repository="' + r.name + '" onclick="getCommits(this)">Get Commits</a> - <a href="#" data-username="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
   document.getElementById("repositories").innerHTML = repoList
 }
 
