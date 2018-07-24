56cda6bd14c7b590faefb9ce4f446c156e6c9f00
Dan Suarez <suarez.daniel101@gmail.com>
2018-07-24 23:32:28 +0000

    Done.

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ function getRepositories() {
   req.send()
 }
 
-
 function displayRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repository="' + r.name + '" onclick="getCommits(this)">Get Commits</a> - <a href="#" data-username="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
-  document.getElementById("repositories").innerHTML = repoList
+  document.getElementById("details").innerHTML = repoList
 }
+
+// function showRepositories(event, data) {
+//   var repos = JSON.parse(this.responseText)
+//   console.log(repos)
+//   const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+//   document.getElementById("repositories").innerHTML = repoList
+// }
 
 function getCommits(el) {
   const name = el.dataset.repository
