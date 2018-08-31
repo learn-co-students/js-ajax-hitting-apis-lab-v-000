a2b7c81fbc1b7849897f124ab52612f0547a7292
Victoria Meng <nights1ststar@protonmail.com>
2018-08-31 02:25:40 +0000

    Done.

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ function getRepositories() {
 function displayRepositories() {
   const repos = JSON.parse(this.responseText);
   console.log(repos);
-  const repoList = `${repos.map(r => `${r.html_url}`)}`;
+  const repoList = `${repos.map(r => r.html_url)}`;
   document.getElementById("repositories").innerHTML = repoList;
+}
+
+function getCommits(el) {
+  const req = new XMLHttpRequest();
+  const repo = el.dataset.repository;
+  const user = el.dataset.username;
+  req.addEventListener("load", showCommits);
+  req.open("GET", `https://api.github.com/repos/${user}/${repo}/commits`);
+  req.send();
+}
+
+function showCommits () {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`;
+  document.getElementById("commits").innerHTML = commitsList;
 }
