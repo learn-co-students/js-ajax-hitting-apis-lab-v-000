22793da2dc274cb770543ed542f688de62f5598c
tsyaeger <tsyaeger@gmail.com>
2018-08-31 18:44:55 -0700

    Done.

--- index.js
+++ index.js
@@ -10,10 +10,11 @@ function showRepositories(event, data) {
 }
 
  
-function getRepositories() {
+function getRepositories() { 
+  let username = document.getElementById('username').value
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories);
-  req.open("GET", 'https://api.github.com/users/tsyaeger/repos')
+  req.open("GET", 'https://api.github.com/users/' + username + '/repos')
   req.send()
 }
 
