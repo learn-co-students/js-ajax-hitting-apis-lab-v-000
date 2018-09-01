eb27fe464d362bc84eb0a405a50db4e293c3c655
tsyaeger <tsyaeger@gmail.com>
2018-08-31 18:14:55 -0700

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 function showRepositories(event, data) {
   var repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a>' + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Branches</a></li>').join('')}</ul>`
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a>' + ' - <a href="#" data-repo="' + r.name + '" onclick="getBranches(this)">Get Branches</a></li>').join('')}</ul>`
 
   document.getElementById("repositories").innerHTML = repoList 
 }
@@ -31,4 +31,21 @@ function showCommits() {
   const commits = JSON.parse(this.responseText)
   const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
   document.getElementById("commits").innerHTML = commitsList
+}
+
+
+
+function getBranches(el) {
+  const name = el.dataset.repo
+  const req = new XMLHttpRequest()
+  req.addEventListener("load", showBranches)
+  req.open("GET", 'https://api.github.com/repos/tsyaeger/' + name + '/branches')
+  req.send()
+}
+
+
+function showBranches() {
+  const branches = JSON.parse(this.responseText)
+  const branchesList = `<ul>${branches.map(branch => '<li>' + branch.name + '</li>').join('')}</ul>`
+  document.getElementById("branches").innerHTML = branchesList
 }
