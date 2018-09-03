e6e5cb2662d4d615cc971660f00bcb9f758ded60
github username <github email address>
2018-09-03 14:33:08 -0400

    saftey

--- index.js
+++ index.js
@@ -3,29 +3,32 @@ function getRepositories() {
   let username = document.getElementById('username').value
   let req = new XMLHttpRequest()
     req.addEventListener("load", displayRepositories);
-    req.open("GET", `https://api.github.com/users/elaineparie/repos`)
+    req.open("GET", `https://api.github.com/users/${username}/repos`)
     req.send()
 }
 
   function displayRepositories() {
     let repos = JSON.parse(this.responseText)
-     const repoList = `<ul>${repos.map(r => '<li>' + 'Repo Name:' + r.full_name + 'Link:' + r.html_url + '</li>').join('')}</ul>`
-     console.log(repoList)
-  }
-//  document.getElementById("repositories").innerHTML = repoList
-//}
-//
-//
-//function getCommits(el) {
-//  const name = el.dataset.repo
-//  const req = new XMLHttpRequest()
-//  req.addEventListener("load", showCommits)
-//  req.open("GET", 'https://api.github.com/repos/octocat/' + name + '/commits')
-//  req.send()
-//}
+     const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" data-username="' + r.owner.login + '" onclick="getCommits(this)"> Get Commits</a></li>').join('')}</ul>`
 
-//function displayCommits() {
-//  const commits = JSON.parse(this.responseText)
-//  const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
-//  document.getElementById("commits").innerHTML = commitsList
-//}
+
+     document.getElementById("repositories").innerHTML = repoList
+  }
+
+  function getCommits(el) {
+    const name = el.dataset.repo
+    console.log(name)
+    const repo = el.dataset.repository
+    const username = el.dataset.username
+    console.log(username)
+    const req = new XMLHttpRequest()
+    req.addEventListener("load", displayCommits)
+    req.open("GET", 'https://api.github.com/repos/octocat/' + username + '/commits')
+    req.send()
+  }
+
+  function displayCommits() {
+    const commits = JSON.parse(this.responseText)
+    const commitsList = `<ul>${commits.map(commit => '<li><strong>' + commit.author.login + '</strong> - ' + commit.commit.message + '</li>').join('')}</ul>`
+    document.getElementById("commits").innerHTML = commitsList
+  }
