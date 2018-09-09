956122ec25607972d0a8db6eff452fe71d527222
wlycdgrfromflatiron <wlycdgrfromflatiron@gmail.com>
2018-09-08 23:41:13 -0400

    Implement retrieving and displaying a user's repositories

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
+// AJAX CALLERS
 function getRepositories(){
     const username = document.getElementById('username').value;
-    console.log(username);
+
+    const req = new XMLHttpRequest();
+
+    req.addEventListener("load", displayRepositories);
+
+    req.open(
+        'GET',
+        `https://api.github.com/users/${username}/repos`
+    );
+
+    req.send();
+}
+
+// AJAX CALLBACKS
+function displayRepositories(event, data){
+    const reposJSON = JSON.parse(this.responseText);
+
+    reposList = 
+        '<ul>' +
+            reposJSON.map(repo => `<li><a href="${repo.html_url}" target="blank">${repo.name}</a></li>`).join('') +
+        '</ul>';
+
+    document.getElementById('repositories').innerHTML = reposList;
 }
