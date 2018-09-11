1b314c680a06698e42bd4105fd81d4218e4cfd54
Emily Zhao <tong.zhao.92@gmail.com>
2018-09-11 16:17:18 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // your code here
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  let user = document.getElementById("username").value
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', `https://api.github.com/users/${user}/repos`)
+  req.send()
+}
+
+function displayRepositories() {
+  var repos = JSON.parse(this.responseText)
+  debugger;
+  const repoList = `<ul>` + repos.forEach(function(element) {
+    return `<li> <a href=`${element.html_url}`>${element.name}</a></li>`
+  }) + `</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
