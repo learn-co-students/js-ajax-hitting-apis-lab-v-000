62a858cdc84512546fcb68612d97c919d140066f
Sonia Duarte Nicas <sduarte22@gmail.com>
2018-10-15 11:00:06 -0700

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function getRepositories(){
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos')
+  req.send();
+}
+
+function displayRepositories(){
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList
+}
+
+
+function getsCommits(repo{
+  
+}
