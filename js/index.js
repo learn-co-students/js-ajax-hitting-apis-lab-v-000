57fb290af194e5171b404376bf6677f48040f857
github username <github email address>
2018-11-01 06:15:16 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -22,8 +22,8 @@ function displayRepositories(){
       <li>
         <h2>${repo.name}</h2>
         <h3>${repo.html_url}</h3>
-        <a href="#" data-repo="${repo.name}" data-username="${repo.owner.login}" onclick="getCommits(this)">Get Commits</a><br>
-        <a href="#" onclick="getBranches(this)">Get Branches</a>
+        <a href="#" data-repository="${repo.name}" data-username="${repo.owner.login}" onclick="getCommits(this)">Get Commits</a><br>
+        <a href="#" data-repository="${repo.name}" data-username="${repo.owner.login}" onclick="getBranches(this)">Get Branches</a>
       </li>`;
     }).join('') +
   '</ul>'
@@ -33,10 +33,10 @@ function displayRepositories(){
 function getCommits(el){
   // User data-repo above because we want to grab the data repositories
   // We'd use data-set if we wanted to grab the sets and below variable would change to dataset.set
-  const name = el.dataset.repo;
+  const repository = el.dataset.repository;
   const req = new XMLHttpRequest();
   const username = el.dataset.username
-  const url = 'https://api.github.com/repos/' + username + '/' + name + '/commits'
+  const url = 'https://api.github.com/repos/' + username + '/' + repository + '/commits'
   req.addEventListener('load', displayCommits);
   // I need to write GET /repos/:owner/:repo/commits below
   // I had to add grab the username from repo.owner.login and store it in data-username but not sure why
@@ -59,23 +59,19 @@ function displayCommits(){
 }
 
 function getBranches(el){
-  // Add a link to each repository that calls a `getBranches` function - done
-  // when clicked and, when complete, calls a `displayBranches` function that fills the
-  // `details` div with a list of names of each branch of the repository.
-  // Give the link data attributes of `username` and `repository` for use by the `getBranches` function
+  const username = el.dataset.username;
+  const repository = el.dataset.repository;
+  const url = "https://api.github.com/repos/" + username + "/" + repository + "/branches";
   const req = new XMLHttpRequest();
-  var username = el.dataset.username;
-  var repository = el.dataset.repository
-  var url = "https://api.github.com/repos/" + username + "/" + repository + "/branches"
   req.addEventListener('load', displayBranches);
-  debugger;
-  console.log(req.open('GET', url));
+  req.open('GET', url);
   req.send();
-  // /repos/:owner/:repo/branches/:branch
-
 }
 
 function displayBranches(){
   const branches = JSON.parse(this.responseText)
-  console.log(branches);
+  const branchesList = `<ul>${branches.map(
+    branch => '<li>' + branch.name + '</li>'
+  ).join('')}</ul>`
+  document.getElementById('details').innerHTML = branchesList;
 }
