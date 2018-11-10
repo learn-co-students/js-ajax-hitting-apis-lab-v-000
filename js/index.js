706a82ce68d64fdadcc91a415c87671d83123a29
amckean12 <amckean@me.com>
2018-11-10 12:45:51 -0600

    Done.

--- js/index.js
+++ js/index.js
@@ -1,16 +1,48 @@
 // your code here
 function displayCommits(){
+  let commits = JSON.parse(this.responseText);
+  let commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><h3>' +
+        commit.commit.author.name +
+        ' (' +
+        commit.author.login +
+        ')</h3>' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
 }
-function displayBranches(){
+function displayBranches(el){
+  let branches = JSON.parse(this.responseText);
+  let branchesList = `<ul>${branches
+  .map(branch => '<li>' + branch.name + '</li>')
+  .join('')}</ul>`;
+document.getElementById('details').innerHTML = branchesList;
 }
 
 function displayRepositories(){
   let repos = JSON.parse(this.responseText);
-  console.log(repos);
-  const repoList = `<ul>${repos
-    .map(r => '<li>' + r.name + '</li>')
-    .join('')}</ul>`;
-  document.getElementById('repositories').innerHTML = repoList;
+  const repoList =
+  '<ul>' +
+  repos
+    .map(repo => {
+      const dataUsername = 'data-username="' + repo.owner.login + '"';
+      const dataRepoName = 'data-repository="' + repo.name + '"';
+      return `
+        <li>
+          <h2>${repo.name}</h2>
+          <a href="${repo.html_url}">${repo.html_url}</a><br>
+          <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+          <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+        </li>`;
+    })
+    .join('') +
+  '</ul>';
+document.getElementById('repositories').innerHTML = repoList;
+
 }
 
 function getRepositories(){
@@ -22,14 +54,20 @@ function getRepositories(){
   xhr.send();
 }
 
-function getBranches(){
+function getBranches(el){
   let xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.github.com/repos/:owner/:repo/branches')
+  //What does this mean?
+  let repoName = el.dataset.repository
+  let username = document.getElementById('username').value
+  xhr.open('GET', `https://api.github.com/repos/${username}/${repoName}/branches`)
   xhr.send();
 }
 
-function getCommits(){
+function getCommits(el){
   let xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.github.com/repos/:owner/:repo/commits')
+  //What does this mean?
+  let repoName = el.dataset.repository
+  let username = document.getElementById('username').value
+  xhr.open('GET', `https://api.github.com/repos/${username}/${repoName}/commits`)
   xhr.send();
 }
