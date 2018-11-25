0dfd4e0f4b2df5483c24268d898879f3df8e6d67
ElleeB <laura.trager@gmail.com>
2018-11-25 11:03:49 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -9,11 +9,9 @@ function getRepositories() {
 function displayRepositories() {
   let repos = JSON.parse(this.responseText)
   console.log(repos)
-  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ` - <a href="${r.html_url}">`+ r.html_url + '</a>' + '<br>').join('')}</ul>`
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ` - <a href="${r.html_url}">`+ r.html_url + '</a>' + '<br>' + `<a href="#" data-username="${r.owner.login}" data-repository="${r.name}" onclick="getCommits(this)">Get Commits</a>` + '<br>' + `<a href="#" data-username="${r.owner.login}" data-repository="${r.name}" onclick="getBranches(this)">Get Branches</a>` + '</li>' + '<br>').join('')}</ul>`
   document.getElementById('repositories').innerHTML = repoList
 }
-// /https:\/\/github.com\/octocat\/Hello-World/
-// + `<a href="#" data-username="${r.owner.login}" data-repository="${r.name}" onclick="getCommits(this)">Get Commits</a>` + '</li>' + '<br>')
 
 function getCommits(el) {
   let repoName = el.dataset.repository
@@ -28,6 +26,22 @@ function displayCommits() {
   let commits = JSON.parse(this.responseText)
   console.log(commits)
   const commitsList = `<ul>${commits.map(commit => '<li>' + `${commit.author.login}` + '/' + `${commit.commit.committer.name}` + '/' + `${commit.commit.message}` + '</li>').join("")}</ul>`
-  console.log(commitsList)
   document.getElementById('details').innerHTML = commitsList
+}
+
+function getBranches(el) {
+  let repoName = el.dataset.repository
+  let user = el.dataset.username
+  const req = new XMLHttpRequest()
+  req.addEventListener('load', displayBranches)
+  req.open('GET', `https://api.github.com/repos/${user}/${repoName}/branches`)
+  req.send()
+}
+
+function displayBranches() {
+  // fills the details div with a list of names of each branch of the repo
+  let branches = JSON.parse(this.responseText)
+  console.log(branches)
+  const branchesList = `<ul>${branches.map(branch => '<li>' + `${branch.name}` + '</li>').join("")}`
+  document.getElementById('details').innerHTML = branchesList
 }
