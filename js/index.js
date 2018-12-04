1eafc47f4119e9f9c714044acb310af0addad78b
github username <github email address>
2018-12-04 11:32:58 -0500

    in process

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // your code here
+
+function displayRepositories(){
+  let repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `${repos.map(repo => '<li>' + repo.name + repo.html_url +'</li>')} `;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+
+
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+
+function getCommits(el) {
+
+};
