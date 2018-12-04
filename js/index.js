faf2e6276d6e6601bfabb0125f6064afc6456d31
github username <github email address>
2018-12-04 15:50:37 -0500

    still in process

--- js/index.js
+++ js/index.js
@@ -1,15 +1,5 @@
 // your code here
 
-function displayRepositories(){
-  let repos = JSON.parse(this.responseText);
-  console.log(repos);
-  const repoList = `${repos.map(repo => '<li>' + repo.name + repo.html_url +'</li>')} `;
-  document.getElementById('repositories').innerHTML = repoList;
-}
-
-
-
-
 function getRepositories() {
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayRepositories);
@@ -17,7 +7,31 @@ function getRepositories() {
   req.send();
 }
 
+function displayRepositories(){
+  let repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos.map(repo => '<li>' +
+  repo.name + ',' + '   ' +
+  '- <a href="#" data-repo=' +
+  repo.name + '" onclick="getCommits(this)">Get Commits</a>' +
+  repo.html_url +'</li>')
+  .join('')} </ul> `;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+
+
 
 function getCommits(el) {
-
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  console.log(name);
+  req.send();
 };
+
+function displayCommits(){
+
+
+}
