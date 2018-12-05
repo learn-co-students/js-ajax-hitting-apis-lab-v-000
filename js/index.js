2dee7a8cb594b145fe2b56914b9d11cccf45572d
github username <github email address>
2018-12-04 19:21:37 -0500

    still working on getCommits

--- js/index.js
+++ js/index.js
@@ -1,21 +1,22 @@
 // your code here
 
 function getRepositories() {
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', displayRepositories);
-  req.open('GET', 'https://api.github.com/users/octocat/repos');
-  req.send();
+  const request = new XMLHttpRequest();
+  request.addEventListener('load', displayRepositories);
+  request.open('GET', 'https://api.github.com/users/octocat/repos');
+  request.send();
 }
 
 function displayRepositories(){
   let repos = JSON.parse(this.responseText);
   console.log(repos);
   const repoList = `<ul>${repos.map(repo => '<li>' +
-  repo.name + ',' + '   ' +
+  repo.name  + '   ' +
   '- <a href="#" data-repo=' +
-  repo.name + '" onclick="getCommits(this)">Get Commits</a>' +
+  repo.name + '" onclick="getCommits(this)">Get Commits</a>'+ '  '+
   repo.html_url +'</li>')
   .join('')} </ul> `;
+
   document.getElementById('repositories').innerHTML = repoList;
 }
 
@@ -23,15 +24,19 @@ function displayRepositories(){
 
 
 function getCommits(el) {
-  const name = el.dataset.repo;
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', displayCommits);
-  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  let name = el.dataset.repo;
+  const request = new XMLHttpRequest();
+  request.addEventListener('load', displayCommits);
+  request.open('GET', 'https://api.github.com/repos/octocat/'+ name +'/commits');
   console.log(name);
-  req.send();
+  request.send();
 };
 
 function displayCommits(){
-
+//   const commits = JSON.parse(this.responseText);
+//   const commitsList = `<ul>${commits.map(commit => '<li>' commit.author.name + commit.author.full_name + commit.message + '</li>')}
+// ).join('')}</ul>`;
+//
+//   document.getElementById('details').innerHTML = commitList;
 
 }
