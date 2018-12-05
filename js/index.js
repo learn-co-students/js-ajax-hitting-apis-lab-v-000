481d9599ee85395574480c4299b2d9cc81fd8254
github username <github email address>
2018-12-05 17:53:37 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -11,17 +11,22 @@ function getRepositories() {
 
 function displayRepositories(){
   let repos = JSON.parse(this.responseText);
-  console.log(repos);
-  const repoList = `<ul>${repos.map(repo=>'<li>' +
-  repo.name  + '   ' +
-  '- <a href="#" data-repository="' +
-  repo.name + '" onclick="getCommits(this)">Get Commits</a>'+ '  '+
-  repo.html_url +'</li>')
-  .join('')} </ul> `;
+  // console.log(repos);
+
+  const repoList = '<ul>'+ repos.map(repo =>{
+    const dataUserName = 'data-username="' + repo.owner.login + '"';
+    const dataRepo = 'data-repository="' + repo.name + '"';
+    return`
+      <li>
+      <h4>${repo.name}</h4>
+      <a href="${repo.html_url}">${repo.html_url}</a><br><br>
+      <a href="#" ${dataRepo} ${dataUserName} onclick="getCommits(this)"> Get Commits</a><br>
+      <a href="#" ${dataRepo} ${dataUserName} onclick="getBranches(this)"> Get Branches</a><br>
+      </li>`;
+    }).join('')+ '</ul>';
 
   document.getElementById('repositories').innerHTML = repoList;
 }
-
 
 function getCommits(el) {
   const name = el.dataset.repository;
@@ -37,7 +42,7 @@ function displayCommits(){
   const commits = JSON.parse(this.responseText);
 
   const commitList = `<ul>${commits.map(commit=>'<li>'+
-  commit.author.login + '   ' + commit.commit.author.name +  ' ' + commit.commit.message +
+  commit.author.login + ' -  ' + commit.commit.author.name +  ' - ' + commit.commit.message +
   '</li>')
   .join('')}</ul>`;
 
