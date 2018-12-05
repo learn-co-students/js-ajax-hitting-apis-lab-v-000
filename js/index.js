7d0922859aeb74644b528067d1551d91fff4d74c
github username <github email address>
2018-12-05 13:55:08 -0500

    while passsed tests, still need to add branch link to displayRepositores

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
 // your code here
 
 function getRepositories() {
+  let username = document.getElementById('username').value;
   const request = new XMLHttpRequest();
+
   request.addEventListener('load', displayRepositories);
-  request.open('GET', 'https://api.github.com/users/octocat/repos');
+  request.open('GET',`https://api.github.com/users/${username}/repos`);
   request.send();
 }
 
 function displayRepositories(){
   let repos = JSON.parse(this.responseText);
   console.log(repos);
-  const repoList = `<ul>${repos.map(repo => '<li>' +
+  const repoList = `<ul>${repos.map(repo=>'<li>' +
   repo.name  + '   ' +
-  '- <a href="#" data-repo=' +
+  '- <a href="#" data-repository="' +
   repo.name + '" onclick="getCommits(this)">Get Commits</a>'+ '  '+
   repo.html_url +'</li>')
   .join('')} </ul> `;
@@ -21,22 +23,42 @@ function displayRepositories(){
 }
 
 
-
-
 function getCommits(el) {
-  let name = el.dataset.repo;
+  const name = el.dataset.repository;
+
   const request = new XMLHttpRequest();
   request.addEventListener('load', displayCommits);
   request.open('GET', 'https://api.github.com/repos/octocat/'+ name +'/commits');
-  console.log(name);
+  console.log('repoName',name);
   request.send();
 };
 
 function displayCommits(){
-//   const commits = JSON.parse(this.responseText);
-//   const commitsList = `<ul>${commits.map(commit => '<li>' commit.author.name + commit.author.full_name + commit.message + '</li>')}
-// ).join('')}</ul>`;
-//
-//   document.getElementById('details').innerHTML = commitList;
+  const commits = JSON.parse(this.responseText);
 
+  const commitList = `<ul>${commits.map(commit=>'<li>'+
+  commit.author.login + '   ' + commit.commit.author.name +  ' ' + commit.commit.message +
+  '</li>')
+  .join('')}</ul>`;
+
+  document.getElementById('details').innerHTML = commitList;
+}
+
+function getBranches(el) {
+  let username = el.dataset.username;
+  const name = el.dataset.repository;
+  const request = new XMLHttpRequest();
+  request.addEventListener('load', displayBranches);
+  request.open('GET',`https://api.github.com/repos/${username}/${name}/branches`);
+  request.send();
+}
+
+function displayBranches(){
+  const branches = JSON.parse(this.responseText);
+
+  const branchList = `<ul>${branches.map(branch=>'<li>'+
+  branch.name + '</li>')
+  .join('')}</ul>`;
+
+  document.getElementById('details').innerHTML = branchList;
 }
