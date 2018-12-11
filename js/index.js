598daed178ccf6ae973b9d1095b123a1f17b8fcb
vickicello <vicki.cello@gmail.com>
2018-12-11 14:44:39 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,63 @@
-// your code here
+function getRepositories() {
+  const name =  document.getElementById("username").value
+  const req = new XMLHttpRequest();
+  req.addEventListener('load',displayRepositories);
+  req.open('GET', `https://api.github.com/users/${name}/repos`);
+  req.send();
+}
+
+function displayRepositories() {
+  var repos = JSON.parse(this.responseText);
+  const name =  document.getElementById("username").value
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+       '<li>' +
+      r.html_url +
+      '<a href="#" data-repository="' + r.name +
+      '" onclick="getCommits(this)">Display Commits</a></li>'
+   )
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+ function getCommits(el) {
+  const user = document.getElementById("username").value
+  const name = el.dataset.repository;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('GET', `https://api.github.com/repos/${user}/` + name + `/commits`);
+  req.send();
+}
+
+ function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits.map(commit =>
+       '<li><strong>' +
+       commit.commit.author.name + commit.url +
+      '</strong> - ' +
+       commit.commit.message +
+       '</li>'
+    ).join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+}
+
+function getBranches(el) {
+  const user = document.getElementById("username").value
+  const name = el.dataset.repository;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('GET', `https://api.github.com/repos/${user}/` + name + `/branches`);
+  req.send();
+}
+
+ function displayBranches() {
+  const branches = JSON.parse(this.responseText);
+   const commitsList = `<ul>${branches.map(branch =>
+       '<li><strong>' +
+       branch.name +
+      '</strong>' +
+       '</li>'
+    ).join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+}
