4353bae1cfac7bf0b8d96bd032fcf7b57192bcdc
Bryan Hooper <bryan@lindsayfarm.net>
2019-02-01 16:45:17 -0500

    all tests passing!

--- js/index.js
+++ js/index.js
@@ -11,7 +11,7 @@ function getRepositories() {
 
 function displayRepositories() {
   const repos = JSON.parse(this.responseText);
-  const repoList = `<ul>${repos.map(repo => '<li style="color: grey;"><a href="' + repo.html_url + '">' + repo.name + '</a> - <a id="' + repo.name + '"href="#" data-username="' + repo.owner.login + '" data-repository="' + repo.name + '" onclick="getCommits(this);">Get Commits</a></li>').join('')}</ul>`;
+  const repoList = `<ul>${repos.map(repo => '<li style="color: grey;"><a href="' + repo.html_url + '">' + repo.name + '</a> - <a id="' + repo.name + '"href="#" data-username="' + repo.owner.login + '" data-repository="' + repo.name + '" onclick="getCommits(this);">Get Commits</a> | <a href="#" onclick="getBranches(this);" data-username="' + repo.owner.login + '" data-repository="' + repo.name + '">Get Branches</a></li>').join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
 
@@ -29,11 +29,21 @@ function displayCommits() {
   let commits = JSON.parse(this.responseText);
   const commitsList = `<ul>${commits.map(c => '<li>' + c.commit.author.name + ' | ' + c.author.login + ' | ' + c.commit.message + '</li>').join('')}</ul>`;
   document.getElementById('details').innerHTML = commitsList;
-
-
 }
 
-function getBranches() {
+function getBranches(el) {
+  const name = el.dataset.username;
+  const repo = el.dataset.repository;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayBranches);
+  req.open('GET', `https://api.github.com/repos/${name}/${repo}/branches`);
+  req.send();
+}
+
+function displayBranches() {
+  let branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches.map(b => '<li>' + b.name + '</li>').join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchesList;
 }
 
 
