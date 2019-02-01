6d0b933a9d3c2098b948303bef493fe2d0d4d30c
epic0448 <samuelogundiran@Yahoo.com>
2019-02-01 10:45:35 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,42 @@
+import { Z_PARTIAL_FLUSH } from "zlib";
+
 // your code here
+const rootURL = "https://api.github.com"
 
 function getRepositories(){
+    const name = document.getElementById("username").value
+    const uri = rootURL + "/users" + name + "/repos"
+    const xhr = new XMLHttpRequest()
+    xhr.addEventListener("load", displayRepositories)
+    xhr.open("GET", uri)
+    xhr.send()
+    //var repos = JSON.parse(this.responseText)
+}
+
+function displayCommits(){
+    const details = JSON.parse(this.responseText);
+    const detailsList = `<ul>${details.map
+        (
+            detail => '<li><strong>' + octocat +
+            '</strong> - ' + 
+            detail.commit.message +
+            '</li>'
+        )
+    .join('')}</ul>`;
+    document.getElementById('details').innerHTML = detailsList;
+}
+
+
+function diisplayBraches(){
     
 }
+
+
+
+
+
+
+
+
+
+
