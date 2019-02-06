f47fa16ca8978655df9bfb73073247450bd37137
github username <github email address>
2019-02-05 20:14:10 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // your code here
+function getRepositories() {
+  const username = document.getElementById('username').value;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/' + username + '/repos');
+  req.send();
+}
+
+function showRepositories() {
+  var repos = JSON.parse(this.responseText);
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + '</li>').join('')}</ul>`;
+
+  document.getElementById('repositories').innerHTML = repoList;
+}
