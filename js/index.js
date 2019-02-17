a002245471c2d48737bb4d8c7311c82e90bd7437
emanuelbierman <emanuel.bierman@gmail.com>
2019-02-17 17:33:00 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,55 @@
 // your code here
+function showRepositories() {
+  //this is set to the XMLHttpRequest object that fired the event
+  // const username = document.getElementById("username").value;
+  var repos = JSON.parse(this.responseText);
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        `<li>
+          <a href=${r.html_url}>${r.name}</a><br>
+          <a href="#" data-repository=${r.name}
+          onclick="getCommits(this)">Get Commits</a><br>
+          <a href="#" data-repository=${r.name}
+          onclick="getBranches(this)">Get Branches</a><br>
+        </li>`
+    )
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getRepositories() {
+  const username = document.getElementById("username").value;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', `https://api.github.com/users/${username}/repos`);
+  req.send();
+}
+
+function getCommits(el) {
+  const username = document.getElementById("username").value;
+  const commits_url = `https://api.github.com/repos/${username}/${el.dataset.repository}/commits`;
+  const reqq = new XMLHttpRequest();
+  reqq.addEventListener('load', displayCommits);
+  reqq.open('GET', commits_url);
+  reqq.send();
+}
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      c => `<li><strong>${c.commit.author.name}</strong>/${c.author.login} - ${c.commit.message}</li>`
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+}
+
+function getBranches() {
+
+}
+
+function displayBranches() {
+
+}
