a2efb52ae3480f41231671104d0fc2089354c60f
emanuelbierman <emanuel.bierman@gmail.com>
2019-02-17 17:45:54 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // your code here
-function showRepositories() {
+function displayRepositories() {
   //this is set to the XMLHttpRequest object that fired the event
   // const username = document.getElementById("username").value;
   var repos = JSON.parse(this.responseText);
@@ -22,7 +22,7 @@ function showRepositories() {
 function getRepositories() {
   const username = document.getElementById("username").value;
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
+  req.addEventListener('load', displayRepositories);
   req.open('GET', `https://api.github.com/users/${username}/repos`);
   req.send();
 }
@@ -46,10 +46,21 @@ function displayCommits() {
   document.getElementById('details').innerHTML = commitsList;
 }
 
-function getBranches() {
-
+function getBranches(el) {
+  const username = document.getElementById("username").value;
+  const branches_url = `https://api.github.com/repos/${username}/${el.dataset.repository}/branches`;
+  const reqq = new XMLHttpRequest();
+  reqq.addEventListener('load', displayCommits);
+  reqq.open('GET', branches_url);
+  reqq.send();
 }
 
 function displayBranches() {
-
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches
+    .map(
+    b => `<li><strong>${b.name}</strong></li>`
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchesList;
 }
