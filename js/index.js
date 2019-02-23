53a5eca971dbf5e9e9d1e91cb4e1e85ce14fca0b
Bruce Hillsworth <bruce.hillsworth@gmail.com>
2019-02-23 04:58:43 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -2,7 +2,8 @@ function getRepositories() {
   let username = document.getElementById("username").value;
   let repos = `https://api.github.com/users/${username}/repos`;
 
-  req = new XMLHttpRequest();
+  let req = new XMLHttpRequest();
+  req.addEventListener("load", displayRepositories);
   req.open("GET", repos);
   req.send();
 }
@@ -31,5 +32,9 @@ function displayBranches() {
 
 
 function displayRepositories() {
+  let repos = JSON.parse(this.responseText);
+  console.log(repos);
+  let repoList = `<ul>${repos.map(r => "<li>" + r.name + "<a href="${r.html_url}">" + "</li>").join(" ")}</ul>`;
 
+  document.getElementById('repositories').innerHTML = repoList;
 }
