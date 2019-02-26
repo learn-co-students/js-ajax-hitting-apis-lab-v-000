3ceac73abc9119a3c736338ca922e249ebeb3bd9
ellefaraday <faraday.elle@gmail.com>
2019-02-25 20:50:29 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // your code here
+function getRepositories(){
+  let username = document.getElementById("username").value;
+  const req = new XMLHttpRequest();
+  req.addEventListener("load", showRespositories);
+  req.open('GET', 'https://api.github.com/users/'+ username +'/repos');
+  req.send();
+}
+
+function showRespositories(){
+  let repos = JSON.parse(this.responseText);
+  let repoList = `<ul>${repos.map(r =>
+  '<li>'+ r.name + '</li>').join(" ")}</ul>`;
+  document.getElementById('repositories').innerHTML += repoList;
+}
