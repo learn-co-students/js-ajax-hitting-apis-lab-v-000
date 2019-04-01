63513a07033acdebf5d7c9e79414f6ccab1c7924
atxrenegade <harleighabel@gmail.com>
2019-04-01 17:49:29 -0500

    Done.

--- js/index.js
+++ js/index.js
@@ -10,18 +10,24 @@ function getRepositories(){
 }
 
 function displayRepositories(){
-	let repos = JSON.parse(this.responseText);
-	console.log(repos);
-	const repoList = `<ul>${repos
-		.map(
-			repo => '<li>' + repo.name + ' - <a href="#" data-repo="' +
-				repo.name +
-				'" onclick="getCommits(this)">Get Commits</a>' + ' - ' +
-				'<a href="#" data-repo="' +
-					repo.name +
-				'"onclick="getBranches(this)">Get Branches</a>' + '</li>'
-		)
-		.join('')}</ul>`;
+		let repos = JSON.parse(this.responseText);
+		console.log(repos);
+		const repoList =
+	    '<ul>' +
+	    repos
+	      .map(repo => {
+	        const dataUsername = 'data-username="' + repo.owner.login + '"';
+	        const dataRepoName = 'data-repository="' + repo.name + '"';
+	        return `
+	          <li>
+	            <h2>${repo.name}</h2>
+	            <a href="${repo.html_url}">${repo.html_url}</a><br>
+	            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+	            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+	          </li>`;
+	      })
+	      .join('') +
+	'</ul>';
 
   document.getElementById('repositories').innerHTML = repoList;
 }
