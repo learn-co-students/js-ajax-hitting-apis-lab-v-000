43b906c18c5456b61db4c34d391e2d7990e4a58a
github username <github email address>
2019-05-01 16:43:23 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // your code here
+function displayCommits(el){
+  const commits  = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+  .map(
+    commit =>
+    '<li><strong>' +
+     commit.author.login +
+     '</strong> - ' +
+      commit.commit.message +
+      '</li>'
+  ).join('')}</ul>`;
+  document.getElementById('Details').innerHTML = commitsList;
+}
