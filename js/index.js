b33ee1f8743c871d6e8614c74a1c8f0b74eb3dc3
harleyharl <harleymteden@gmail.com>
2019-05-14 07:09:20 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+const rootURL = 'https://api.github.com';
+
 function retrieveGithubUsername(){
   return document.getElementById('username').value
 }
@@ -32,9 +34,50 @@ function displayRepositories() {
 }
 
 function getCommits(element){
-  console.log(element)
+  const repoName = element.dataset.repository
+  const uri = rootURL + '/repos/' + element.dataset.username + '/' + repoName + '/commits';
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('GET', uri);
+  req.send();
 }
 
 function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+      .map(
+        commit =>
+        '<li><h3>' +
+        '/' +
+        commit.author.login +
+        '/' +
+        commit.commit.committer.name +
+        '</h3>' +
+        commit.commit.message +
+        '</li>'
+      )
+      .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+}
 
+function getBranches(element){
+  const repoName = element.dataset.repository
+  const uri = rootURL + '/repos/' + element.dataset.username + '/' + repoName + '/branches'
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayBranches);
+  req.open('GET', uri);
+  req.send();
+}
+
+function displayBranches(){
+  branches = JSON.parse(this.responseText)
+  const branchesList = `<ul>${branches
+      .map(
+        branch =>
+        '<li><h3>' +
+        branch.name +
+        '</h3></li>'
+      )
+      .join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchesList;
 }
