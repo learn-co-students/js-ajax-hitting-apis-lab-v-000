b3c9b79a0d170c01c4f9205eda4b7ac0da4d5911
harleyharl <harleymteden@gmail.com>
2019-05-13 18:13:59 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// your code here
+function retrieveGithubUsername(){
+  return document.getElementById('username').value
+}
+
+function getRepositories() {
+  const usernameInput = retrieveGithubUsername()
+  const req = new XMLHttpRequest();
+  req.open('GET', `https://api.github.com/users/${usernameInput}/repos`);
+  req.send();
+}
