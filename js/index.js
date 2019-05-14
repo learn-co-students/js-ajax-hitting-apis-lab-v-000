aa875ef11839168731c56a2fee6aebb9f61fb47d
harleyharl <harleymteden@gmail.com>
2019-05-13 22:16:39 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -2,15 +2,6 @@ function retrieveGithubUsername(){
   return document.getElementById('username').value
 }
 
-function displayRepositories() {
-  var repos = JSON.parse(this.responseText);
-  // console.log(repos)
-  let repoList = `<ul>${repos
-    .map(r =>'<b>' + r.name + '</b>' + '<br>' + '<li>' + r.html_url + '</li>' + `<a onclick="getCommits(this);" href='#'>Commits<a>` + '<br><br>')
-    .join('')} </ul>`;
-  document.getElementById('repositories').innerHTML = repoList;
-}
-
 function getRepositories() {
   const usernameInput = retrieveGithubUsername()
   const req = new XMLHttpRequest();
@@ -19,15 +10,29 @@ function getRepositories() {
   req.send();
 }
 
-function getCommits(event) {
-  console.log(this)
-  // return event
-  // http://api.github.com/repos/octocat/Hello-World/commits
-  // const usernameInput = retrieveGithubUsername()
-  // const req = new XMLHttpRequest();
-  // req.addEventListener('load', displayRepositories);
-  // req.open('GET', `https://api.github.com/users/${usernameInput}/repos`);
-  // req.send();
+function displayRepositories() {
+  const repos = JSON.parse(this.responseText);
+  const repoList =
+    '<ul>' +
+    repos
+      .map(repo => {
+        const dataUsername = 'data-username="' + repo.owner.login + '"';
+        const dataRepoName = 'data-repository="' + repo.name + '"';
+        return `
+          <li>
+            <h2>${repo.name}</h2>
+            <a href="${repo.html_url}">${repo.html_url}</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
+            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
+          </li>`;
+      })
+      .join('') +
+    '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getCommits(element){
+  console.log(element)
 }
 
 function displayCommits() {
