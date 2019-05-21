bc80da78367334946aa4d8796838bada19683948
RonSala <ron.sala@gmail.com>
2019-05-21 01:29:22 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -21,6 +21,25 @@ function showRepositories() {
   document.getElementById('repositories').innerHTML = repoList;
 }
 
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/:username/' + name + '/commits');
+  req.send();
+}
+
 function displayCommits() {
-  
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('commits').innerHTML = commitsList;
 }
