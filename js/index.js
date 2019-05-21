6e9f7fb26d8de91accf5eb18cedc3390a65cf9e1
RonSala <ron.sala@gmail.com>
2019-05-21 03:15:28 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
-function getRepositories() {
-  const req = new XMLHttpRequest();
-  req.addEventListener('load', showRepositories);
-  req.open('GET', 'https://api.github.com/users/:username/repos');
-  req.send();
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li><strong>' +
+        commit.author.login +
+        '</strong> - ' +
+        commit.commit.message +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
 }
 
-function showRepositories() {
+function displayBranches() {
+
+}
+
+function displayRepositories() {
   var repos = JSON.parse(this.responseText);
-  console.log(repos);
+  // console.log(repos);
   const repoList = `<ul>${repos
     .map(
       r =>
@@ -21,25 +33,23 @@ function showRepositories() {
   document.getElementById('repositories').innerHTML = repoList;
 }
 
+function getRepositories() {
+  debugger
+  var username = document.getElementById('username').value;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', 'https://api.github.com/users/' + username +'/repos');
+  req.send();
+}
+
 function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
-  req.addEventListener('load', showCommits);
+  req.addEventListener('load', displayCommits);
   req.open('GET', 'https://api.github.com/repos/:username/' + name + '/commits');
   req.send();
 }
 
-function displayCommits() {
-  const commits = JSON.parse(this.responseText);
-  const commitsList = `<ul>${commits
-    .map(
-      commit =>
-        '<li><strong>' +
-        commit.author.login +
-        '</strong> - ' +
-        commit.commit.message +
-        '</li>'
-    )
-    .join('')}</ul>`;
-  document.getElementById('commits').innerHTML = commitsList;
+function getBranches() {
+
 }
