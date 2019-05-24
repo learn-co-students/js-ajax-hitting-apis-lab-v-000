6dcadb30231dc47f07b2980cd7ffd385b581952c
Erin Green <erin.y.green@gmail.com>
2019-05-24 20:43:34 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // your code here
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  const username = document.getElementById("username").value
+
+  req.addEventListener('load', showRepositories);
+  req.open('GET', `https://api.github.com/users/${username}/repos`);
+  req.send
+}
+
+function showRepositories() {
+  let repos = JSON.parse(this.responseText);
+  console.log(repos)
+
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.name + '</li>')
+    .join('')}</ul>`;
+    
+  document.getElementById('repositories').innerHTML = repoList;
+}
