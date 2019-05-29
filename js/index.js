c570cab80a82b713743740ac159fa333c0b9150f
Erin Green <erin.y.green@gmail.com>
2019-05-29 00:43:34 +0000

    Done.

--- js/index.js
+++ js/index.js
@@ -43,13 +43,14 @@ function getCommits(el) {
 
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
+
   const commitsList = `<ul>${commits
     .map(
       commit =>
         '<li><h3>' +
         commit.commit.author.name +
-        ' (' +
-        commit.author.login +
+        ' ('
+        + commit.author.login +
         ')</h3>' +
         commit.commit.message +
         '</li>'
@@ -66,6 +67,7 @@ function getBranches(el) {
   xhr.open('GET', 'https://api.github.com/repos/' + el.dataset.username + '/' + repoName + '/branches');
   xhr.send();
 }
+
 function displayBranches() {
   const branches = JSON.parse(this.responseText);
   const branchesList = `<ul>${branches
