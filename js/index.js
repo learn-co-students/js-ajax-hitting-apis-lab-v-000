ddbcdbcdf96a2dc442f26a65eaaaaee32cebef7e
MargaritaKhodzka <mkhodzka@gmail.com>
2019-06-12 16:47:45 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,81 @@
-// your code here
+function getRepositories() {
+  const username = document.getElementById('username').value;
+  const req = new XMLHttpRequest();
+  const uri = 'https://api.github.com/users/' + username + '/repos';
+
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', uri);
+  req.send();
+  return false;
+};
+
+function displayRepositories() {
+  let repos = JSON.parse(this.responseText);
+
+  const repoList =
+    '<ul>' +
+    repos
+      .map(repo => {
+        const userName = 'data-username="' + repo.owner.login + '"';
+        const repoName = 'data-repository="' + repo.name + '"';
+        return `<li>
+          ${repo.name}
+          <a href="${repo.html_url}">${repo.html_url}</a><br>
+          <a href="#" ${repoName} ${userName} onclick="getCommits(this)">Get Commits</a> -
+          <a href="#" ${repoName} ${userName} onclick="getBranches(this)">Get Branches</a>
+        </li>`;
+      }).join('') +
+    '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
+};
+
+function getCommits(el) {
+  const name = el.dataset.repository;
+  const req = new XMLHttpRequest();
+  const uri = 'https://api.github.com/repos/' + el.dataset.username + '/' + name + '/commits';
+
+  req.addEventListener('load', displayCommits);
+  req.open('GET', uri);
+  req.send();
+};
+
+
+function displayCommits() {
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+      '<li><strong>' +
+      commit.commit.author.name +
+      ' - ' +
+      commit.author.login +
+      '</strong> - ' +
+      commit.commit.message +
+      '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+};
+
+function getBranches(el) {
+  const name = el.dataset.repository;
+  const req = new XMLHttpRequest();
+  const uri = 'https://api.github.com/repos/' + el.dataset.username + '/' + name + '/branches';
+
+  req.addEventListener('load', displayBranches);
+  req.open('GET', uri);
+  req.send();
+};
+
+function displayBranches() {
+  const branches = JSON.parse(this.responseText);
+  const branchesList = `<ul>${branches
+    .map(
+      branch =>
+      '<li>' +
+      branch.name +
+      '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchesList;
+};
