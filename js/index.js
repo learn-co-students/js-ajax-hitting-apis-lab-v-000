d9b8da2797983289f12b64cf69c62d434fd11549
ryanmanchester <ryanm17@gmail.com>
2019-06-12 12:55:56 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 function displayRepositories() {
   let repos = JSON.parse(this.responseText);
-
   const repoList = `<ul>${repos
     .map(
       r =>
         '<li>' +
-        r.name + ` - <a href= "${r.html_url}">${r.html_url}</a>` +
-        ' - <a href="#" data-username="'+ r.owner.login + '"data-repo="' +
+        r.name + ` - <a href= "${r.html_url}">${r.name}</a>` +
+        ' - <a href="#" data-username="'+ r.owner.login + '"data-repository="' +
         r.name +
-        '" onclick="getCommits(this)">Get Commits</a></li>'
+        '" onclick="getCommits(this)">Get Commits</a>' + ' - <a href="#" data-username="'+ r.owner.login + '"data-repository="' +
+        r.name + '"onclick="getBranches(this)">Get Branches</a></li>'
     )
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
 
 function getRepositories() {
-const userName = document.querySelector('input').value;
+const userName = document.getElementById('username').value;
 const req = new XMLHttpRequest();
 req.addEventListener('load', displayRepositories);
 req.open('GET', `https://api.github.com/users/${userName}/repos`);
@@ -23,21 +23,22 @@ req.send();
 }
 
 function getCommits(el) {
-  const elName = el.dataset.repo;
+  const repoName = el.dataset.repository;
   const userName = el.dataset.username;
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayCommits);
-  req.open('GET', `https://api.github.com/repos/${userName}/` + elName + '/commits');
+  req.open('GET', `https://api.github.com/repos/${userName}/${repoName}/commits`);
   req.send();
 }
 
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
-  debugger;
+
   const commitsList = `<ul>${commits
     .map(
       c =>
         '<li><strong>' +
+        c.commit.author.name +
         c.author.login +
         '</strong> - ' +
         c.commit.message +
@@ -45,4 +46,22 @@ function displayCommits() {
     )
     .join('')}</ul>`;
   document.getElementById('details').innerHTML = commitsList;
+}
+
+function getBranches(el) {
+  const repoName = el.dataset.repository;
+  const userName = el.dataset.username;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayBranches);
+  req.open('GET', `https://api.github.com/repos/${userName}/${repoName}/branches`);
+  req.send();
+}
+
+function displayBranches() {
+  const branches = JSON.parse(this.responseText);
+  let branchName = `<ul>${branches.map(
+    b =>
+    '<li><strong>' + b.name + '</strong></li>'
+  ).join('')}</ul>`;
+  document.getElementById('details').innerHTML = branchName;
 }
