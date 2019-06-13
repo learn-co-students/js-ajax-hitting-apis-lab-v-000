6732c7da94abf7657f14b4a87a74f1eb84de1519
Ashley <ashleywong678@gmail.com>
2019-06-13 19:17:22 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
-// your code here
+function displayCommits(){
+  const commits = JSON.parse(this.responseText);
+  const commitsList = `<ul>${commits
+    .map(
+      commit =>
+        '<li>' +
+        commit.commit.author.name + ' - ' + commit.commit.message + ' ' + commit.committer.login +
+        '</li>'
+    )
+    .join('')}</ul>`;
+  document.getElementById('details').innerHTML = commitsList;
+}
+
+function displayBranches() {
+
+}
+
+function displayRepositories(){
+  var repos = JSON.parse(this.responseText);
+  const repoList = `<ul>${repos
+    .map(r => '<li>' + r.clone_url + '- <a href"#" data-repository="' + r.repository +'</li>')
+    .join('')}</ul>`;
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getRepositories(){
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
+}
+
+function getCommits(el){
+  const name = el.dataset.repository;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
+
+function getBranches(){
+
+}
