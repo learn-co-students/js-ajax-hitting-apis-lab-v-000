44deae7ffa2f4294dc7c109c43380a65e289f27b
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-07-05 17:11:07 -0700

    Done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-// your code here
+// function displayCommits() {}
+//
+// function displayBranches() {}
+
+function displayRepositories() {
+  let repoList = '<ul>';
+  for (let i = 0; i < this.responseText.length; i++) {
+    repoList += '<li>' + this.responseText[i]['name'] + '</li>';
+  }
+  repoList += '</ul>';
+  document.getElementById('repositories').innerHTML = repoList;
+}
+
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
+  user = $('input[id=username]').value
+  req.open('GET', `https://api.github.com/users/${user}/repos`);
+  req.send();
+}
+
+// function getCommits() {}
+//
+// function getBranches() {}
